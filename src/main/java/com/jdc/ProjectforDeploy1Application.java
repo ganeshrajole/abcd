bf6370ab8fc4a1e7b76263823fb88fe9d021c3f5
Ganesh Rajole <ganeshrajole@gmail.com>
2022-11-12 12:12:04 +0530

    2nd commit

---
+++ src/main/java/com/jdc/ProjectforDeploy1Application.java
@@ -0,0 +1,13 @@
+package com.jdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectforDeploy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectforDeploy1Application.class, args);
+	}
+
+}
